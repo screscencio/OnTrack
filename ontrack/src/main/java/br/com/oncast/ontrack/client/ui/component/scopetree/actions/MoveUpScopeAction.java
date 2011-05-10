b4680435a81946e6667e6476674e6257166fe97f
ontrack <ontrack@014e18b7-1377-44f3-b398-c31593b6cf9d>
2011-05-10 13:32:43 +0000

    Added widgets actions: Move up and down.
    git-svn-id: http://ic.ontrack/svn/repos/trunk@11 014e18b7-1377-44f3-b398-c31593b6cf9d

--- ontrack/src/main/java/br/com/oncast/ontrack/client/ui/component/scopetree/actions/MoveUpScopeAction.java
+++ ontrack/src/main/java/br/com/oncast/ontrack/client/ui/component/scopetree/actions/MoveUpScopeAction.java
@@ -11,14 +11,18 @@ public class MoveUpScopeAction implements ScopeAction {
 	}
 
 	@Override
-	public void execute() {
-		if (selectedScope.isRoot()) return;
+	public void execute() throws UnableToCompleteActionException {
+		if (selectedScope.isRoot()) throw new UnableToCompleteActionException("It is not possible to move a root node.");
 
 		final int index = selectedScope.getIndex();
-		if (index == 0) return;
+		if (isFirstNode(index)) throw new UnableToCompleteActionException("It is not possible to move up the first node.");
 
 		final Scope parent = selectedScope.getParent();
 		parent.remove(selectedScope);
 		parent.add(index - 1, selectedScope);
+	}
+
+	private boolean isFirstNode(final int index) {
+		return index == 0;
 	}
 }
---
+++ ontrack/src/main/java/br/com/oncast/ontrack/client/ui/component/scopetree/widget/actions/MoveDownScopeTreeWidgetAction.java
@@ -0,0 +1,36 @@
+package br.com.oncast.ontrack.client.ui.component.scopetree.widget.actions;
+
+import br.com.oncast.ontrack.client.ui.component.scopetree.actions.UnableToCompleteActionException;
+import br.com.oncast.ontrack.client.ui.component.scopetree.widget.ScopeTreeItem;
+
+import com.google.gwt.user.client.ui.Tree;
+import com.google.gwt.user.client.ui.TreeItem;
+
+public class MoveDownScopeTreeWidgetAction implements ScopeTreeWidgetAction {
+
+	private final ScopeTreeItem treeItem;
+
+	public MoveDownScopeTreeWidgetAction(final ScopeTreeItem treeItem) {
+		this.treeItem = treeItem;
+	}
+
+	@Override
+	public void execute() throws UnableToCompleteActionException {
+		if (treeItem.isRoot()) throw new UnableToCompleteActionException("It is not possible to move a root node.");
+
+		final Tree tree = treeItem.getTree();
+
+		final TreeItem parentItem = treeItem.getParentItem();
+		final int index = parentItem.getChildIndex(treeItem);
+		if (isLastNode(index, parentItem)) throw new UnableToCompleteActionException("It is not possible to move down the last node.");
+
+		treeItem.remove();
+		parentItem.insertItem(index + 1, treeItem);
+
+		tree.setSelectedItem(treeItem);
+	}
+
+	private boolean isLastNode(final int index, final TreeItem parentItem) {
+		return parentItem.getChildCount() - 1 == index;
+	}
+}
