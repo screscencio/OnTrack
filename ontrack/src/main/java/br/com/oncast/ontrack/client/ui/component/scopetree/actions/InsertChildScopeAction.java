c3040a0c2682fce9a8fb0d4911373c5045bdb4f0
ontrack <ontrack@014e18b7-1377-44f3-b398-c31593b6cf9d>
2011-05-12 14:26:34 +0000

    Jaime: Insert child action created.
    git-svn-id: http://192.168.2.95/svn/repos/trunk@30 014e18b7-1377-44f3-b398-c31593b6cf9d

---
+++ ontrack/src/main/java/br/com/oncast/ontrack/client/ui/component/scopetree/actions/InsertChildScopeAction.java
@@ -0,0 +1,30 @@
+package br.com.oncast.ontrack.client.ui.component.scopetree.actions;
+
+import br.com.oncast.ontrack.client.ui.component.scopetree.exceptions.UnableToCompleteActionException;
+import br.com.oncast.ontrack.shared.beans.Scope;
+
+public class InsertChildScopeAction implements InsertionScopeAction {
+
+	private final Scope selectedScope;
+	private final Scope newScope;
+
+	public InsertChildScopeAction(final Scope selectedScope) {
+		this.selectedScope = selectedScope;
+		newScope = new Scope("");
+	}
+
+	@Override
+	public void execute() throws UnableToCompleteActionException {
+		selectedScope.add(newScope);
+	}
+
+	@Override
+	public Scope getScope() {
+		return selectedScope;
+	}
+
+	@Override
+	public Scope getNewScope() {
+		return newScope;
+	}
+}
