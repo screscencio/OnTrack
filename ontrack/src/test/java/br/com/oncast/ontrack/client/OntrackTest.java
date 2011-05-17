cf67168899d99651b9cdacbbc7f8d1ef50d21a6e
ontrack <ontrack@014e18b7-1377-44f3-b398-c31593b6cf9d>
2011-05-17 20:08:44 +0000

    Jaime: created structure for testing views using gwt-test-utils.
    git-svn-id: http://192.168.2.95/svn/repos/trunk@38 014e18b7-1377-44f3-b398-c31593b6cf9d

---
+++ ontrack/src/test/java/br/com/oncast/ontrack/client/OntrackTest.java
@@ -0,0 +1,26 @@
+package br.com.oncast.ontrack.client;
+
+
+public class OntrackTest {
+
+	// @Test
+	// public void test() {
+	// final ScopeTreeWidget scopeTree = new ScopeTreeWidget(new ScopeTreeWidgetInteractionHandler() {
+	// @Override
+	// public void onItemUpdate(final ScopeTreeItem item, final String newContent) {}
+	//
+	// @Override
+	// public void onKeyUp(final KeyUpEvent event) {}
+	// });
+	//
+	// final Scope scope = new Scope("1");
+	// final ScopeTreeItem rootItem = new ScopeTreeItem(scope);
+	// scopeTree.add(rootItem);
+	//
+	// assertTrue(true);
+	// // // ActionManager.execute(new MoveUpScopeAction(scope), scopeTree);
+	// //
+	// // // assertArrayEquals(modifiedTree, scopeTree);
+	// // // assertArrayEquals(modifiedScope, scope);
+	// }
+}
