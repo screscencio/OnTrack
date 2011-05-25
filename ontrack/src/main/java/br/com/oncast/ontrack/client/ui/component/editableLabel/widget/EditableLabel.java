ce42cb129bc7cd9b28388a49ddee4ee5b669d582
ontrack <ontrack@014e18b7-1377-44f3-b398-c31593b6cf9d>
2011-05-25 13:45:20 +0000

    Luiz: Adding release tag to editableLabel
    git-svn-id: http://ic.ontrack/svn/repos/trunk@66 014e18b7-1377-44f3-b398-c31593b6cf9d

--- ontrack/src/main/java/br/com/oncast/ontrack/client/ui/component/editableLabel/widget/EditableLabel.java
+++ ontrack/src/main/java/br/com/oncast/ontrack/client/ui/component/editableLabel/widget/EditableLabel.java
@@ -13,8 +13,6 @@ import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.event.shared.HandlerRegistration;
-import com.google.gwt.regexp.shared.MatchResult;
-import com.google.gwt.regexp.shared.RegExp;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.Timer;
@@ -126,8 +124,10 @@ public class EditableLabel extends Composite implements HasValue<String> {
 
 	@Override
 	public void setValue(final String value) {
-		final MatchResult exec = RegExp.compile("[a-zA-Z0-9][^@?][a-zA-Z0-9]*").exec(value);
+		// final MatchResult exec = RegExp.compile("[a-zA-Z0-9][^@?][a-zA-Z0-9]*").exec(value);
 		// TODO make regex.
+		editLabel.setText(value);
+		editBox.setText(value);
 	}
 
 	@Override
