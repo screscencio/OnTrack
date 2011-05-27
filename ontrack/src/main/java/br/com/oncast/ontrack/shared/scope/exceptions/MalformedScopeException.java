214ea78ee6e58ba251cf053d5f73902819e19b21
ontrack <ontrack@014e18b7-1377-44f3-b398-c31593b6cf9d>
2011-05-27 17:05:33 +0000

    Jaime: created a class to parse a description and translate it into a scope description and a release description.
    git-svn-id: http://ic.ontrack/svn/repos/trunk@74 014e18b7-1377-44f3-b398-c31593b6cf9d

---
+++ ontrack/src/main/java/br/com/oncast/ontrack/shared/scope/exceptions/MalformedScopeException.java
@@ -0,0 +1,10 @@
+package br.com.oncast.ontrack.shared.scope.exceptions;
+
+public class MalformedScopeException extends RuntimeException {
+
+	private static final long serialVersionUID = -7083164649210647907L;
+
+	public MalformedScopeException(final String message) {
+		super(message);
+	}
+}
