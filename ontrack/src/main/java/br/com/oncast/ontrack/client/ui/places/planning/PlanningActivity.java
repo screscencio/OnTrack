d9ed0d94e907cfae40e3348cc50ee9b0b865b3d3
ontrack <ontrack@014e18b7-1377-44f3-b398-c31593b6cf9d>
2011-05-31 12:25:08 +0000

    Jaime: PlanningActionExecutionRequestHandler extracted to a separated class.
    git-svn-id: http://192.168.2.95/svn/repos/trunk@79 014e18b7-1377-44f3-b398-c31593b6cf9d

--- ontrack/src/main/java/br/com/oncast/ontrack/client/ui/places/planning/PlanningActivity.java
+++ ontrack/src/main/java/br/com/oncast/ontrack/client/ui/places/planning/PlanningActivity.java
@@ -1,14 +1,8 @@
 package br.com.oncast.ontrack.client.ui.places.planning;
 
-import java.util.List;
-
 import br.com.oncast.ontrack.client.mocks.ProjectMockFactory;
-import br.com.oncast.ontrack.client.ui.components.scopetree.actions.ActionExecutionListener;
-import br.com.oncast.ontrack.client.ui.components.scopetree.actions.ActionExecutionRequestHandler;
-import br.com.oncast.ontrack.client.ui.components.scopetree.actions.ActionManager;
 import br.com.oncast.ontrack.shared.project.Project;
 import br.com.oncast.ontrack.shared.project.ProjectContext;
-import br.com.oncast.ontrack.shared.scope.actions.ScopeAction;
 
 import com.google.gwt.activity.shared.AbstractActivity;
 import com.google.gwt.event.shared.EventBus;
@@ -19,43 +13,10 @@ public class PlanningActivity extends AbstractActivity {
 	private Project mockedProject;
 	private ProjectContext mockedProjectContext;
 
-	private final class PlanningActionExecutionRequestHandler implements ActionExecutionRequestHandler, ActionExecutionListener {
-
-		private final ActionManager actionManager;
-		private final List<ActionExecutionListener> actionExecutionSuccessHandlers;
-
-		public PlanningActionExecutionRequestHandler(final List<ActionExecutionListener> actionExecutionSuccessHandlers) {
-			actionManager = new ActionManager(this);
-			this.actionExecutionSuccessHandlers = actionExecutionSuccessHandlers;
-		}
-
-		@Override
-		public void onActionExecutionRequest(final ScopeAction action) {
-			actionManager.execute(action, getProjectContext());
-		}
-
-		@Override
-		public void onActionUndoRequest() {
-			actionManager.undo(getProjectContext());
-		}
-
-		@Override
-		public void onActionRedoRequest() {
-			actionManager.redo(getProjectContext());
-		}
-
-		@Override
-		public void onActionExecution(final ScopeAction action, final boolean wasRollback) {
-			for (final ActionExecutionListener handler : actionExecutionSuccessHandlers) {
-				handler.onActionExecution(action, wasRollback);
-			}
-		}
-	}
-
 	@Override
 	public void start(final AcceptsOneWidget panel, final EventBus eventBus) {
 		final PlanningView view = new PlanningPanel();
-		view.setActionExecutionRequestHandler(new PlanningActionExecutionRequestHandler(view.getActionExecutionSuccessListeners()));
+		view.setActionExecutionRequestHandler(new PlanningActionExecutionRequestHandler(getProjectContext(), view.getActionExecutionSuccessListeners()));
 
 		final Project project = getProject();
 		view.setScope(project.getScope());
