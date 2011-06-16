41898e84950ed10f79d0da0e42bd5720ffb48660
ontrack <ontrack@014e18b7-1377-44f3-b398-c31593b6cf9d>
2011-06-16 19:24:45 +0000

    Luiz: Refactoring ScopeRemoveAction to remove and revert child itens of a removed item.
    git-svn-id: http://192.168.2.95/svn/repos/trunk@143 014e18b7-1377-44f3-b398-c31593b6cf9d

--- ontrack/src/main/java/br/com/oncast/ontrack/shared/scope/actions/ScopeRemoveAction.java
+++ ontrack/src/main/java/br/com/oncast/ontrack/shared/scope/actions/ScopeRemoveAction.java
@@ -1,46 +1,74 @@
 package br.com.oncast.ontrack.shared.scope.actions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import br.com.oncast.ontrack.shared.project.ProjectContext;
 import br.com.oncast.ontrack.shared.release.Release;
 import br.com.oncast.ontrack.shared.scope.Scope;
 import br.com.oncast.ontrack.shared.scope.exceptions.UnableToCompleteActionException;
 import br.com.oncast.ontrack.shared.util.uuid.UUID;
 
-// TODO Implement algorithm for remove child scope association with their releases.
 public class ScopeRemoveAction implements ScopeAction {
 
-	private Scope selectedScope;
-	private Scope parent;
+	private UUID selectedScopeId;
+	private List<ScopeRemoveAction> childList;
+	private UUID parentScopeId;
 	private int index;
+	private String description;
 	private Release release;
 
 	public ScopeRemoveAction(final Scope selectedScope) {
-		this.selectedScope = selectedScope;
+		this.selectedScopeId = selectedScope.getId();
+		this.childList = new ArrayList<ScopeRemoveAction>();
 	}
 
 	protected ScopeRemoveAction() {}
 
 	@Override
 	public void execute(final ProjectContext context) throws UnableToCompleteActionException {
+		final Scope selectedScope = context.findScope(selectedScopeId);
+		final Scope parent = selectedScope.getParent();
+		if (parent == null) { throw new UnableToCompleteActionException("Unable to remove root level."); }
+
+		this.parentScopeId = parent.getId();
+		this.description = selectedScope.getDescription();
+
 		if (selectedScope.isRoot()) throw new UnableToCompleteActionException("It is not possible to remove a root node.");
-		parent = selectedScope.getParent();
+
+		for (final Scope child : selectedScope.getChildren()) {
+			final ScopeRemoveAction childRemoved = new ScopeRemoveAction(child);
+			childList.add(childRemoved);
+		}
+
+		for (final ScopeRemoveAction childRemoved : childList) {
+			childRemoved.execute(context);
+		}
+
 		index = parent.getChildIndex(selectedScope);
 		parent.remove(selectedScope);
 		release = selectedScope.getRelease();
+
 		if (release != null) release.removeScope(selectedScope);
 		selectedScope.setRelease(null);
 	}
 
 	@Override
 	public void rollback(final ProjectContext context) throws UnableToCompleteActionException {
-		// TODO save all childs id and restore here.
-		parent.add(index, selectedScope);
-		selectedScope.setRelease(release);
-		if (release != null) release.addScope(selectedScope);
+		final Scope parent = context.findScope(parentScopeId);
+		final Scope newScope = new Scope(description, selectedScopeId);
+
+		parent.add(index, newScope);
+		newScope.setRelease(release);
+		if (release != null) release.addScope(newScope);
+
+		for (final ScopeRemoveAction childId : childList) {
+			childId.rollback(context);
+		}
 	}
 
 	@Override
 	public UUID getReferenceId() {
-		return selectedScope.getId();
+		return selectedScopeId;
 	}
 }
