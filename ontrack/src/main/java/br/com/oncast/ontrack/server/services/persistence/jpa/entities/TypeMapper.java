9f127a065ed5ea44a56b1a5559191e26fe6319c5
ontrack <ontrack@014e18b7-1377-44f3-b398-c31593b6cf9d>
2011-06-29 17:53:43 +0000

    Jaime: refactoring entity translator. (Just before a big refactoring)
    git-svn-id: http://ic.ontrack/svn/repos/trunk@169 014e18b7-1377-44f3-b398-c31593b6cf9d

---
+++ ontrack/src/main/java/br/com/oncast/ontrack/server/services/persistence/jpa/entities/TypeMapper.java
@@ -0,0 +1,30 @@
+package br.com.oncast.ontrack.server.services.persistence.jpa.entities;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TypeMapper {
+
+	void map(final Object action, final ModelActionEntity actionEntity, final Field field) throws IllegalAccessException, NoSuchFieldException {
+		final Type type = field.getGenericType();
+		if (type instanceof ParameterizedType) {
+			final ParameterizedType pt = (ParameterizedType) type;
+
+			final Type[] typeArguments = pt.getActualTypeArguments();
+			if (typeArguments.length > 1) throw new RuntimeException("The mapping of beans doesn't support more than one generic type at the same property.");
+
+			final List childList = new ArrayList((List) field.get(action));
+			final List childEntityList = new ArrayList();
+			for (final Object childAction : childList) {
+				childEntityList.add(BeanMapper.map(childAction));
+			}
+			// FIXME Remove this redundant code
+			final Field entityField = actionEntity.getClass().getDeclaredField(field.getName());
+			entityField.setAccessible(true);
+			entityField.set(actionEntity, childEntityList);
+		}
+	}
+}
