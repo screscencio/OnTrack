603261ee88a7b48baff60f55425b231cdd8849f0
ontrack <ontrack@014e18b7-1377-44f3-b398-c31593b6cf9d>
2011-07-07 18:23:25 +0000

    Jaime: EffortInferenceEngine started.
    git-svn-id: http://ic.ontrack/svn/repos/trunk@207 014e18b7-1377-44f3-b398-c31593b6cf9d

---
+++ ontrack/src/main/java/br/com/oncast/ontrack/shared/model/scope/EffortInferenceEngine.java
@@ -0,0 +1,10 @@
+package br.com.oncast.ontrack.shared.model.scope;
+
+public class EffortInferenceEngine {
+
+	public static Scope updateModel(final Scope scope, final int newEffort) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ ontrack/src/test/java/br/com/oncast/ontrack/shared/model/scope/InferenceEngineTest.java
@@ -0,0 +1,17 @@
+package br.com.oncast.ontrack.shared.model.scope;
+
+import org.junit.Test;
+
+import br.com.oncast.ontrack.mocks.models.ScopeMock;
+
+public class InferenceEngineTest {
+
+	@Test
+	public void shouldChangeParentsEffortIfTheirEffortIsDifferent() {
+		final int newEffort = 0;
+		final Scope root = ScopeMock.getScope();
+		final Scope changed = root.getChild(0).getChild(1);
+
+		EffortInferenceEngine.updateModel(changed, 15);
+	}
+}
