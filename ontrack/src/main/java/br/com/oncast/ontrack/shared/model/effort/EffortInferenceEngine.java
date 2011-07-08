0d79cc15e6edd4965cb060e15ff1316be85afedb
ontrack <ontrack@014e18b7-1377-44f3-b398-c31593b6cf9d>
2011-07-08 13:21:06 +0000

    Lobo: *  Effort inference engine almost finished;
    git-svn-id: http://192.168.2.95/svn/repos/trunk@211 014e18b7-1377-44f3-b398-c31593b6cf9d

--- ontrack/src/main/java/br/com/oncast/ontrack/shared/model/effort/EffortInferenceEngine.java
+++ ontrack/src/main/java/br/com/oncast/ontrack/shared/model/effort/EffortInferenceEngine.java
@@ -1,24 +1,48 @@
 package br.com.oncast.ontrack.shared.model.effort;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import br.com.oncast.ontrack.shared.model.scope.Scope;
 
 public class EffortInferenceEngine {
 
 	public static void process(final Scope scope) {
-
-		// if (scope.isRoot()) return;
-		// final Scope parent = scope.getParent();
 		processBottomUp(scope);
 		processTopDown(scope);
 	}
 
 	private static void processTopDown(final Scope scope) {
+		final float infered = scope.getEffort().getInfered();
+		final float calculated = scope.getEffort().getCalculated();
+		final float difference = infered - calculated;
 
+		if (difference == 0) return;
+		if (difference < 0) throw new RuntimeException("Error while infering efforts.");
+
+		final List<Scope> childrenWithNonDeclaredEfforts = getChildrenWithNonDeclaredEfforts(scope);
+		final int size = childrenWithNonDeclaredEfforts.size();
+		if (size == 0) return;
+
+		final float portion = difference / size;
+		for (final Scope child : childrenWithNonDeclaredEfforts) {
+			// TODO Review the method of calculation - could pre calculate it once using the (infered - sumOfDeclaratedChildren / size)
+			child.getEffort().setCalculated(child.getEffort().getCalculated() + portion);
+			processTopDown(child);
+		}
+	}
+
+	private static List<Scope> getChildrenWithNonDeclaredEfforts(final Scope scope) {
+		final List<Scope> childrenWithNonDeclaredEfforts = new ArrayList<Scope>();
+		for (final Scope child : scope.getChildren())
+			if (!child.getEffort().hasDeclared()) childrenWithNonDeclaredEfforts.add(child);
+
+		return childrenWithNonDeclaredEfforts;
 	}
 
 	private static void processBottomUp(final Scope scope) {
 		final float inferedInitial = scope.getEffort().getInfered();
-		scope.getEffort().setCalculed(calculateEffort(scope));
+		scope.getEffort().setCalculated(calculateEffort(scope));
 		final float inferedFinal = scope.getEffort().getInfered();
 
 		if (inferedFinal == inferedInitial) return;
@@ -34,5 +58,4 @@ public class EffortInferenceEngine {
 
 		return sum;
 	}
-
 }
