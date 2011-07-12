590532d532675e15498ea90a96da5f5ae376b072
ontrack <ontrack@014e18b7-1377-44f3-b398-c31593b6cf9d>
2011-07-12 19:58:11 +0000

    Jaime: Tests for Effort inference.
    git-svn-id: http://192.168.2.95/svn/repos/trunk@225 014e18b7-1377-44f3-b398-c31593b6cf9d

---
+++ ontrack/src/test/java/br/com/oncast/ontrack/shared/model/effort/InferenceEngineTest.java
@@ -0,0 +1,31 @@
+package br.com.oncast.ontrack.shared.model.effort;
+
+import static br.com.oncast.ontrack.shared.model.effort.Util.getModifiedScope;
+import static br.com.oncast.ontrack.shared.model.effort.Util.getOriginalScope;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import br.com.oncast.ontrack.shared.model.project.Project;
+import br.com.oncast.ontrack.shared.model.project.ProjectContext;
+import br.com.oncast.ontrack.shared.model.scope.Scope;
+import br.com.oncast.ontrack.shared.model.scope.actions.ScopeMoveLeftAction;
+import br.com.oncast.ontrack.shared.model.scope.exceptions.UnableToCompleteActionException;
+
+public class InferenceEngineTest {
+
+	@Test
+	public void shouldApplyInferenceWhenMoveScopeLeft() throws UnableToCompleteActionException {
+		final String fileName = "Project2";
+		final Scope original = getOriginalScope(fileName);
+
+		final Scope scope = original.getChild(0).getChild(1);
+		final ScopeMoveLeftAction moveLeftAction = new ScopeMoveLeftAction(scope.getId());
+		moveLeftAction.execute(new ProjectContext(new Project(original, null)));
+
+		EffortInferenceEngine.process(scope);
+
+		assertTrue(original.deepEquals(getModifiedScope(fileName)));
+	}
+
+}
---
+++ ontrack/src/test/java/br/com/oncast/ontrack/shared/model/effort/Util.java
@@ -0,0 +1,24 @@
+package br.com.oncast.ontrack.shared.model.effort;
+
+import java.io.File;
+
+import br.com.oncast.ontrack.server.util.mindmapconverter.MindMapConverter;
+import br.com.oncast.ontrack.shared.model.scope.Scope;
+
+public class Util {
+	static Scope getOriginalScope(final String fileName) {
+		return getScope(fileName + " - original");
+	}
+
+	static Scope getModifiedScope(final String fileName) {
+		return getScope(fileName + " - modified");
+	}
+
+	static Scope getModifiedScope(final String fileName, final int version) {
+		return getScope(fileName + " - modified" + version);
+	}
+
+	static Scope getScope(final String fileName) {
+		return MindMapConverter.convert(new File("src/test/java/br/com/oncast/ontrack/shared/model/effort/" + fileName + ".mm"));
+	}
+}
