b3d70a0e4d7a578c15a58cd4c42c0ba99762a739
ontrack <ontrack@014e18b7-1377-44f3-b398-c31593b6cf9d>
2011-07-12 20:01:54 +0000

    Lobo: * Effort distribution almost done;
    git-svn-id: http://192.168.2.95/svn/repos/trunk@226 014e18b7-1377-44f3-b398-c31593b6cf9d

--- ontrack/src/main/java/br/com/oncast/ontrack/shared/util/number/NumberUtils.java
+++ ontrack/src/main/java/br/com/oncast/ontrack/shared/util/number/NumberUtils.java
@@ -2,7 +2,8 @@ package br.com.oncast.ontrack.shared.util.number;
 
 public class NumberUtils {
 
-	public static String roundToStringWithOneFractionalDigit(final float number) {
+	// TODO Review this number formating method;
+	public static String roundToStringMaybeWithOneFractionalDigit(final float number) {
 		final String[] strings = (number + "").split("[.]");
 
 		final char charAt = strings[1].charAt(0);
