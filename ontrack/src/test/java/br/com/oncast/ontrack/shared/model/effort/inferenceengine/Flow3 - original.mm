597d2acd64998faadec4444649b10b9695937229
ontrack <ontrack@014e18b7-1377-44f3-b398-c31593b6cf9d>
2011-07-13 18:54:48 +0000

    Lobo: *  Effort inference engine testing;
    git-svn-id: http://192.168.2.95/svn/repos/trunk@239 014e18b7-1377-44f3-b398-c31593b6cf9d

---
+++ ontrack/src/test/java/br/com/oncast/ontrack/shared/model/effort/inferenceengine/Flow3 - original.mm
@@ -0,0 +1,27 @@
+<map version="1.0.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1310578582920" ID="ID_1386649517" MODIFIED="1310578586536" TEXT="Escopo">
+<node CREATED="1310578629997" ID="ID_517231989" MODIFIED="1310578629997" POSITION="right" TEXT="Cadastros">
+<node CREATED="1310578629997" MODIFIED="1310578629997" TEXT="Cadastros de infra">
+<node CREATED="1310578629997" MODIFIED="1310578629997" TEXT="Cadastro de Usu&#xe1;rios"/>
+<node CREATED="1310578629998" MODIFIED="1310578629998" TEXT="Cadastro de Funcionalidades"/>
+</node>
+<node CREATED="1310578629998" MODIFIED="1310578629998" TEXT="Cadastros de neg&#xf3;cio">
+<node CREATED="1310578629999" MODIFIED="1310578629999" TEXT="Cadastro de Clientes"/>
+<node CREATED="1310578629999" MODIFIED="1310578629999" TEXT="Cadastro de Produtos"/>
+</node>
+</node>
+<node CREATED="1310578629999" ID="ID_1778916004" MODIFIED="1310578629999" POSITION="right" TEXT="Opera&#xe7;&#xf5;es">
+<node CREATED="1310578630000" MODIFIED="1310578630000" TEXT="Colocar produto no carrinho de compras"/>
+<node CREATED="1310578630000" MODIFIED="1310578630000" TEXT="Remover produto do carrinho de compras"/>
+<node CREATED="1310578630001" MODIFIED="1310578630001" TEXT="Fechar pedido"/>
+</node>
+<node CREATED="1310578630001" ID="ID_211134509" MODIFIED="1310578630001" POSITION="right" TEXT="Relat&#xf3;rios">
+<node CREATED="1310578630001" MODIFIED="1310578630001" TEXT="Relat&#xf3;rio de produtos mais vendidos"/>
+<node CREATED="1310578630002" MODIFIED="1310578630002" TEXT="Relat&#xf3;rio de produtos por pedido"/>
+</node>
+<node CREATED="1310578630002" ID="ID_130846388" MODIFIED="1310578630002" POSITION="right" TEXT="Infra-estrutura">
+<node CREATED="1310578630002" MODIFIED="1310578630002" TEXT="Autentica&#xe7;&#xe3;o"/>
+</node>
+</node>
+</map>
