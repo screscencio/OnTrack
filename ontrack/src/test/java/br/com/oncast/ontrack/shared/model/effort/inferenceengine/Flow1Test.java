620880f149621b2ac68c0494bd9f24f2b1b807b4
ontrack <ontrack@014e18b7-1377-44f3-b398-c31593b6cf9d>
2011-07-13 21:08:23 +0000

    Lobo: *  Effort inference engine testing DONE (complete until use phase 8 of use case 1);
    git-svn-id: http://192.168.2.95/svn/repos/trunk@246 014e18b7-1377-44f3-b398-c31593b6cf9d

--- ontrack/src/test/java/br/com/oncast/ontrack/shared/model/effort/inferenceengine/Flow1Test.java
+++ ontrack/src/test/java/br/com/oncast/ontrack/shared/model/effort/inferenceengine/Flow1Test.java
@@ -13,8 +13,8 @@ import br.com.oncast.ontrack.shared.model.scope.exceptions.UnableToCompleteActio
 
 public class Flow1Test {
 
-	private final String FILE_NAME = "Flow1";
-	private final Scope original = getOriginalScope(FILE_NAME);
+	private final String FILE_NAME_PREFIX = "Flow1";
+	private final Scope original = getOriginalScope(FILE_NAME_PREFIX);
 
 	@Test
 	public void shouldApplyInferencesWhenEffortChanges() throws UnableToCompleteActionException {
@@ -23,13 +23,16 @@ public class Flow1Test {
 		shouldRedistributeEffortBetweenChildrenWhenParentEffortIsDeclared();
 		shouldRedistributeEffortBetweenSiblingWhenOneIsDeclared();
 		shouldRedistributeEffortBetweenSiblingWhenInferedChanges();
+		shouldRemoveUnusedInference();
+		shouldChangeAnotherScope1();
+		shouldChangeAnotherScope2();
 	}
 
 	private void shouldApplyInferenceTopDownWhenRootIsModified() {
 		original.getEffort().setDeclared(1000);
 		EffortInferenceEngine.process(original);
 
-		assertDeepEquals(original, getModifiedScope(FILE_NAME, 1));
+		assertDeepEquals(original, getModifiedScope(FILE_NAME_PREFIX, 1));
 	}
 
 	private void shouldRedistributeInferenceBetweenSiblingsWhenOneIsAdded() {
@@ -37,7 +40,7 @@ public class Flow1Test {
 		original.getChild(1).add(newScope);
 		EffortInferenceEngine.process(newScope.getParent());
 
-		assertDeepEquals(original, getModifiedScope(FILE_NAME, 2));
+		assertDeepEquals(original, getModifiedScope(FILE_NAME_PREFIX, 2));
 	}
 
 	private void shouldRedistributeEffortBetweenChildrenWhenParentEffortIsDeclared() {
@@ -49,7 +52,7 @@ public class Flow1Test {
 			assertEquals(87.5, child.getEffort().getInfered(), 0.09);
 		}
 
-		assertDeepEquals(original, getModifiedScope(FILE_NAME, 3));
+		assertDeepEquals(original, getModifiedScope(FILE_NAME_PREFIX, 3));
 	}
 
 	private void shouldRedistributeEffortBetweenSiblingWhenOneIsDeclared() {
@@ -61,7 +64,7 @@ public class Flow1Test {
 		assertEquals(66.6, original.getChild(1).getChild(2).getEffort().getInfered(), 0.09);
 		assertEquals(66.6, original.getChild(1).getChild(3).getEffort().getInfered(), 0.09);
 
-		assertDeepEquals(original, getModifiedScope(FILE_NAME, 4));
+		assertDeepEquals(original, getModifiedScope(FILE_NAME_PREFIX, 4));
 	}
 
 	private void shouldRedistributeEffortBetweenSiblingWhenInferedChanges() {
@@ -81,7 +84,30 @@ public class Flow1Test {
 		assertEquals(133.3, original.getChild(2).getEffort().getInfered(), 0.09);
 		assertEquals(133.3, original.getChild(3).getEffort().getInfered(), 0.09);
 
-		assertDeepEquals(original, getModifiedScope(FILE_NAME, 5));
+		assertDeepEquals(original, getModifiedScope(FILE_NAME_PREFIX, 5));
 	}
 
+	private void shouldRemoveUnusedInference() {
+		final Scope scopeWithChangedEffort = original.getChild(1);
+		scopeWithChangedEffort.getEffort().resetDeclared();
+		EffortInferenceEngine.process(scopeWithChangedEffort.getParent());
+
+		assertDeepEquals(original, getModifiedScope(FILE_NAME_PREFIX, 6));
+	}
+
+	private void shouldChangeAnotherScope1() {
+		final Scope scopeWithChangedEffort = original.getChild(0).getChild(0);
+		scopeWithChangedEffort.getEffort().setDeclared(30);
+		EffortInferenceEngine.process(scopeWithChangedEffort.getParent());
+
+		assertDeepEquals(original, getModifiedScope(FILE_NAME_PREFIX, 7));
+	}
+
+	private void shouldChangeAnotherScope2() {
+		final Scope scopeWithChangedEffort = original.getChild(0).getChild(1);
+		scopeWithChangedEffort.getEffort().setDeclared(60);
+		EffortInferenceEngine.process(scopeWithChangedEffort.getParent());
+
+		assertDeepEquals(original, getModifiedScope(FILE_NAME_PREFIX, 8));
+	}
 }
