56a1eebc6a61bfe73eebfe1237df1b7c9bfd79c3
ontrack <ontrack@014e18b7-1377-44f3-b398-c31593b6cf9d>
2011-07-27 22:17:22 +0000

    Lobo: * Number utils has a better name;
    git-svn-id: http://192.168.2.95/svn/repos/trunk@285 014e18b7-1377-44f3-b398-c31593b6cf9d

--- ontrack/src/main/java/br/com/oncast/ontrack/client/utils/number/ClientDecimalFormat.java
+++ ontrack/src/main/java/br/com/oncast/ontrack/client/utils/number/ClientDecimalFormat.java
@@ -1,6 +1,6 @@
 package br.com.oncast.ontrack.client.utils.number;
 
-public class NumberUtils {
+public class ClientDecimalFormat {
 
 	// TODO Review this number formating method;
 	public static native String roundFloat(final float number, int decimalDigits) /*-{
