2a9bd51c1df7f59ae34b06240e59cb769c279f31
ontrack <ontrack@014e18b7-1377-44f3-b398-c31593b6cf9d>
2011-08-01 18:23:46 +0000

    Lobo: * Inference updated;
    git-svn-id: http://192.168.2.95/svn/repos/trunk@299 014e18b7-1377-44f3-b398-c31593b6cf9d

--- ontrack/src/main/java/br/com/oncast/ontrack/shared/model/scope/inference/ProgressInferenceEngine.java
+++ ontrack/src/main/java/br/com/oncast/ontrack/shared/model/scope/inference/ProgressInferenceEngine.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Set;
 
 import br.com.oncast.ontrack.shared.model.actions.ModelAction;
+import br.com.oncast.ontrack.shared.model.progress.Progress;
 import br.com.oncast.ontrack.shared.model.scope.Scope;
 import br.com.oncast.ontrack.shared.model.uuid.UUID;
 
@@ -18,13 +19,37 @@ public class ProgressInferenceEngine implements InferenceEngine {
 	@Override
 	public Set<UUID> process(final Scope scope) {
 		final HashSet<UUID> inferenceInfluencedScopeSet = new HashSet<UUID>();
-
 		final List<Scope> children = scope.getChildren();
+
+		for (final Scope child : children)
+			checkTopDownDistribution(child, inferenceInfluencedScopeSet);
+
 		for (final Scope child : children)
 			calculateBottomUp(child, inferenceInfluencedScopeSet);
+
 		processBottomUp(scope, inferenceInfluencedScopeSet);
 
 		return inferenceInfluencedScopeSet;
+	}
+
+	private void checkTopDownDistribution(final Scope scope, final HashSet<UUID> inferenceInfluencedScopeSet) {
+		if (scope.isLeaf() || !scope.getProgress().hasDeclared()) return;
+		processTopDownDistribution(scope, scope.getProgress().getDescription(), inferenceInfluencedScopeSet);
+	}
+
+	private void processTopDownDistribution(final Scope scope, final String progressDescription, final HashSet<UUID> inferenceInfluencedScopeSet) {
+		final Progress progress = scope.getProgress();
+
+		if (scope.isLeaf()) {
+			progress.setDescription(progressDescription);
+			inferenceInfluencedScopeSet.add(scope.getId());
+		}
+		else {
+			for (final Scope child : scope.getChildren())
+				processTopDownDistribution(child, progressDescription, inferenceInfluencedScopeSet);
+			progress.reset();
+			calculateBottomUp(scope, inferenceInfluencedScopeSet);
+		}
 	}
 
 	private void processBottomUp(final Scope scope, final HashSet<UUID> inferenceInfluencedScopeSet) {
