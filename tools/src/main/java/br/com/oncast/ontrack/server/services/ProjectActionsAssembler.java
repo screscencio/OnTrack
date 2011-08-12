389cf9390fec6a0fb2b51e2049cc0acda12d97af
ontrack <ontrack@014e18b7-1377-44f3-b398-c31593b6cf9d>
2011-08-12 19:09:07 +0000

    Rodrigo: Also populating releases on MM to Project conversion.
    git-svn-id: http://192.168.2.95/svn/repos/trunk@340 014e18b7-1377-44f3-b398-c31593b6cf9d

---
+++ tools/src/main/java/br/com/oncast/ontrack/server/services/ProjectActionsAssembler.java
@@ -0,0 +1,13 @@
+package br.com.oncast.ontrack.server.services;
+
+import java.util.List;
+
+import br.com.oncast.ontrack.shared.model.actions.ModelAction;
+import br.com.oncast.ontrack.shared.model.project.Project;
+
+public class ProjectActionsAssembler {
+	public static List<ModelAction> assemble(final Project project) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
