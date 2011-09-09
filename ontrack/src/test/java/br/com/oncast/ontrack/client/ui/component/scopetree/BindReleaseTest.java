3647d4642bf671a230a228d00730e23a683a1b80
ontrack <ontrack@014e18b7-1377-44f3-b398-c31593b6cf9d>
2011-09-09 20:33:29 +0000

    Chico/Jaime: Created tests for checking the scope tree update after executing a ScopeBindReleaseAction.
    git-svn-id: http://192.168.2.95/svn/repos/trunk@423 014e18b7-1377-44f3-b398-c31593b6cf9d

---
+++ ontrack/src/test/java/br/com/oncast/ontrack/client/ui/component/scopetree/BindReleaseTest.java
@@ -0,0 +1,116 @@
+package br.com.oncast.ontrack.client.ui.component.scopetree;
+
+import static junit.framework.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.lang.reflect.Field;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import br.com.oncast.ontrack.client.services.actionExecution.ActionExecutionServiceImpl;
+import br.com.oncast.ontrack.client.services.context.ContextProviderService;
+import br.com.oncast.ontrack.client.services.errorHandling.ErrorTreatmentMock;
+import br.com.oncast.ontrack.client.ui.components.scopetree.ScopeTree;
+import br.com.oncast.ontrack.client.ui.components.scopetree.ScopeTreeItem;
+import br.com.oncast.ontrack.client.ui.components.scopetree.widgets.ScopeTreeItemWidget;
+import br.com.oncast.ontrack.client.ui.components.scopetree.widgets.ScopeTreeWidget;
+import br.com.oncast.ontrack.client.ui.generalwidgets.Tag;
+import br.com.oncast.ontrack.mocks.ContextProviderServiceMock;
+import br.com.oncast.ontrack.mocks.models.ReleaseMock;
+import br.com.oncast.ontrack.shared.model.actions.ScopeBindReleaseAction;
+import br.com.oncast.ontrack.shared.model.effort.Effort;
+import br.com.oncast.ontrack.shared.model.project.Project;
+import br.com.oncast.ontrack.shared.model.project.ProjectContext;
+import br.com.oncast.ontrack.shared.model.release.Release;
+import br.com.oncast.ontrack.shared.model.scope.Scope;
+import br.com.oncast.ontrack.shared.model.scope.exceptions.ScopeNotFoundException;
+import br.com.oncast.ontrack.utils.deepEquality.DeepEqualityTestUtils;
+import br.com.oncast.ontrack.utils.deepEquality.custom.mocks.EffortDeepEqualityComparator;
+
+import com.octo.gwt.test.GwtTest;
+
+public class BindReleaseTest extends GwtTest {
+
+	private Scope firstScope;
+	private ScopeTree tree;
+	private ProjectContext projectContext;
+	private ActionExecutionServiceImpl actionExecutionService;
+
+	@BeforeClass
+	public static void beforeClass() {
+		DeepEqualityTestUtils.setCustomDeepEqualityComparator(Effort.class, new EffortDeepEqualityComparator());
+	}
+
+	@AfterClass
+	public static void afterClass() {
+		DeepEqualityTestUtils.removeCustomDeepEqualityComparator(Effort.class);
+	}
+
+	@Before
+	public void setUp() {
+		projectContext = new ProjectContext(new Project(getScope(), ReleaseMock.getRelease()));
+		tree = new ScopeTree();
+		tree.setContext(projectContext);
+
+		final ContextProviderService contextService = new ContextProviderServiceMock(projectContext);
+		actionExecutionService = new ActionExecutionServiceImpl(contextService, new ErrorTreatmentMock());
+		actionExecutionService.addActionExecutionListener(tree.getActionExecutionListener());
+	}
+
+	private Scope getScope() {
+		final Scope rootScope = new Scope("Project");
+		firstScope = new Scope("1");
+		rootScope.add(firstScope);
+		rootScope.add(new Scope("2"));
+
+		return rootScope;
+	}
+
+	@Test
+	public void shouldInsertTagIntoScopeTreeItem() throws Exception {
+		actionExecutionService.onUserActionExecutionRequest(new ScopeBindReleaseAction(firstScope.getId(), "R1"));
+
+		final Tag tag = getTag();
+
+		assertTrue(tag.isVisible());
+		assertEquals("R1", tag.getText());
+	}
+
+	@Test
+	public void shouldRemoveTagFromScopeTreeItemWhenUnbindingScopeFromRelease() throws Exception {
+		final Release release = projectContext.getProjectRelease().getChild(0);
+		release.addScope(firstScope);
+		assertEquals(release, firstScope.getRelease());
+
+		actionExecutionService.onUserActionExecutionRequest(new ScopeBindReleaseAction(firstScope.getId(), ""));
+
+		final Tag tag = getTag();
+
+		assertFalse(tag.isVisible());
+		assertTrue(tag.getText().isEmpty());
+	}
+
+	private Tag getTag() throws ScopeNotFoundException, NoSuchFieldException, IllegalAccessException {
+		final ScopeTreeWidget asWidget = (ScopeTreeWidget) tree.asWidget();
+		final ScopeTreeItem item = asWidget.findScopeTreeItem(firstScope.getId());
+
+		final Field scopeItemWidgetField = item.getClass().getDeclaredField("scopeItemWidget");
+		scopeItemWidgetField.setAccessible(true);
+		final ScopeTreeItemWidget itemWidget = (ScopeTreeItemWidget) scopeItemWidgetField.get(item);
+
+		final Field releaseTagField = itemWidget.getClass().getDeclaredField("releaseTag");
+		releaseTagField.setAccessible(true);
+		final Tag tag = (Tag) releaseTagField.get(itemWidget);
+		return tag;
+	}
+
+	@Override
+	public String getModuleName() {
+		return "br.com.oncast.ontrack.Application";
+	}
+
+}
