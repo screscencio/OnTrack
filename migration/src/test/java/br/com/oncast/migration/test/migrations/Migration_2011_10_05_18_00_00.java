1819ecf923d7daa38a3331b607c7473f2eb62130
ontrack <ontrack@014e18b7-1377-44f3-b398-c31593b6cf9d>
2011-11-03 16:09:23 +0000

    Matsumoto: Migration and MigrationExecuter implemented
    git-svn-id: http://192.168.2.95/svn/repos/trunk@533 014e18b7-1377-44f3-b398-c31593b6cf9d

--- migration/src/test/java/br/com/oncast/migration/test/migrations/Migration_2011_10_05_18_00_00.java
+++ migration/src/test/java/br/com/oncast/migration/test/migrations/Migration_2011_10_05_18_00_00.java
@@ -1,6 +1,8 @@
-package br.com.oncast.migration;
+package br.com.oncast.migration.test.migrations;
 
 import org.dom4j.Document;
+
+import br.com.oncast.migration.Migration;
 
 public class Migration_2011_10_05_18_00_00 extends Migration {
 
