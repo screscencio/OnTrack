a43f59821951a1d9f81fee246707d0a6daf18a91
ontrack <ontrack@014e18b7-1377-44f3-b398-c31593b6cf9d>
2011-11-08 12:26:53 +0000

    Matsumoto: Moved XMLImporter to ontrackExt Project
    git-svn-id: http://192.168.2.95/svn/repos/trunk@549 014e18b7-1377-44f3-b398-c31593b6cf9d

---
+++ ontrack/src/main/java/br/com/oncast/ontrack/server/services/exportImport/xml/XMLImporter.java
@@ -0,0 +1,67 @@
+package br.com.oncast.ontrack.server.services.exportImport.xml;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.simpleframework.xml.Serializer;
+import org.simpleframework.xml.core.Persister;
+
+import br.com.oncast.ontrack.server.business.UserAction;
+import br.com.oncast.ontrack.server.model.Password;
+import br.com.oncast.ontrack.server.services.persistence.PersistenceService;
+import br.com.oncast.ontrack.server.services.persistence.exceptions.PersistenceException;
+import br.com.oncast.ontrack.shared.model.actions.ModelAction;
+import br.com.oncast.ontrack.shared.model.user.User;
+
+public class XMLImporter {
+	
+	private OntrackXML ontrackXML;
+	private final PersistenceService persistanceService;
+	
+	public XMLImporter(PersistenceService persistanceService){
+		this.persistanceService = persistanceService;
+	}
+	
+	public XMLImporter loadXML(String xmlPath){
+		final Serializer serializer = new Persister();
+		final File source = new File(xmlPath);
+		
+		try {
+			ontrackXML = serializer.read(OntrackXML.class, source);
+		} catch (Exception e) {
+			throw new RuntimeException("Unable to deserialize xml file");
+		}
+		return this;
+	}
+	
+	// TODO Verify error treatment
+	public void persistObjects() throws PersistenceException{
+		if(ontrackXML == null) throw new RuntimeException("You must use loadXML method to load xml before use this method.");
+		
+		persistActions(ontrackXML.getUserActions());
+		persistUser(ontrackXML.getUsers());
+		persistPasswords(ontrackXML.getPasswords());
+	}
+
+	private void persistActions(final List<UserAction> userActions) throws PersistenceException {
+		for (final UserAction userAction : userActions) {
+			final ArrayList<ModelAction> arrayList = new ArrayList<ModelAction>();
+			arrayList.add(userAction.getModelAction());
+			persistanceService.persistActions(arrayList, userAction.getTimestamp());
+		}
+	}
+
+	private void persistUser(final List<User> userList) throws PersistenceException {
+		for (final User user : userList) {
+			persistanceService.persistOrUpdateUser(user);
+		}
+	}
+
+	private void persistPasswords(final List<Password> passwordList) throws PersistenceException {
+		for (final Password pass : passwordList) {
+			persistanceService.persistOrUpdatePassword(pass);
+		}
+	}
+	
+}
