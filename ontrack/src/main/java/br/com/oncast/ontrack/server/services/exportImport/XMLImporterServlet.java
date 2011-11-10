162240f37b28b19c59919ea8f216c0097bc77a93
ontrack <ontrack@014e18b7-1377-44f3-b398-c31593b6cf9d>
2011-11-10 12:04:06 +0000

    Jaime / Matsumoto: Added migration execution on received xml before updating database.
    git-svn-id: http://192.168.2.95/svn/repos/trunk@583 014e18b7-1377-44f3-b398-c31593b6cf9d

--- ontrack/src/main/java/br/com/oncast/ontrack/server/services/exportImport/XMLImporterServlet.java
+++ ontrack/src/main/java/br/com/oncast/ontrack/server/services/exportImport/XMLImporterServlet.java
@@ -10,16 +10,24 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.dom4j.Document;
 
 import br.com.oncast.ontrack.server.services.authentication.basic.BasicAutheticator;
+import br.com.oncast.ontrack.server.services.exportImport.xml.MigrationExecuter;
+import br.com.oncast.ontrack.server.services.exportImport.xml.XMLUtils;
 import br.com.oncast.ontrack.server.services.exportImport.xml.abstractions.XMLImporter;
 import br.com.oncast.ontrack.server.services.persistence.exceptions.PersistenceException;
 
 public class XMLImporterServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
+
+	private static final String SOURCE_XML_FILE_NAME = "ontrack.xml";
+	private static final String MIGRATED_XML_FILE_NAME = "migrated.xml";
+	private static final String MIGRATIONS_PACKAGE = "br.com.oncast.ontrack.server.services.exportImport.xml.migrations";
 
 	@Override
 	protected void doPost(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
@@ -28,35 +36,52 @@ public class XMLImporterServlet extends HttpServlet {
 			doReply(request, response);
 		}
 		catch (final Exception e) {
-			doHandleError(request, response, e);
+			// TODO Display an user-friendly error message.
+			throw new ServletException(e);
 		}
+	}
+
+	private void doReply(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+		final File sourceXML = extractXMLFromRequest(request);
+		final File migratedXML = migrate(sourceXML);
+		updateDatabase(migratedXML);
 	}
 
 	@SuppressWarnings("rawtypes")
-	private void doReply(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+	private File extractXMLFromRequest(final HttpServletRequest request) throws FileUploadException, Exception {
 		final ServletFileUpload upload = new ServletFileUpload(new DiskFileItemFactory());
-		final Iterator iter = upload.parseRequest(request).iterator();
+		final Iterator iterator = upload.parseRequest(request).iterator();
 
-		while (iter.hasNext()) {
-			final FileItem fileItem = (FileItem) iter.next();
-			if (!fileItem.isFormField()) updateDatabase(convertToFile(fileItem));
+		while (iterator.hasNext()) {
+			final FileItem fileItem = (FileItem) iterator.next();
+			if (!fileItem.isFormField()) return toFile(fileItem);
 		}
+
+		throw new RuntimeException("Failed to extract xml: there is no xml attached to this request.");
 	}
 
-	// TODO Display an user-friendly error message.
-	private void doHandleError(final HttpServletRequest request, final HttpServletResponse response, final Exception e) throws ServletException {
-		throw new ServletException(e);
-	}
-
-	private File convertToFile(final FileItem item) throws Exception {
-		final File xmlFile = new File("ontrack.xml");
+	private File toFile(final FileItem item) throws Exception {
+		final File xmlFile = new File(SOURCE_XML_FILE_NAME);
 		item.write(xmlFile);
 
 		return xmlFile;
+	}
+
+	private File migrate(final File sourceXML) throws Exception {
+		final MigrationExecuter migrationExecuter = new MigrationExecuter(MIGRATIONS_PACKAGE);
+		final Document document = XMLUtils.read(sourceXML);
+
+		migrationExecuter.executeMigrations(document);
+
+		final File migratedXML = new File(MIGRATED_XML_FILE_NAME);
+		XMLUtils.write(document, migratedXML);
+
+		return migratedXML;
 	}
 
 	private void updateDatabase(final File xmlFile) throws PersistenceException {
 		final XMLImporter xmlImporter = new XMLImporter();
 		xmlImporter.loadXML(xmlFile).persistObjects();
 	}
+
 }
