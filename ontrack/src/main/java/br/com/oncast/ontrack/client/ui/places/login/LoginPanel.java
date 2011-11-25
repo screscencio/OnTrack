952f65e949963e5b4cf8be276a72170b294339e8
ontrack <ontrack@014e18b7-1377-44f3-b398-c31593b6cf9d>
2011-11-25 17:57:31 +0000

    Lobo: * Applying style to the Login screen;
    git-svn-id: http://192.168.2.95/svn/repos/trunk@649 014e18b7-1377-44f3-b398-c31593b6cf9d

--- ontrack/src/main/java/br/com/oncast/ontrack/client/ui/places/login/LoginPanel.java
+++ ontrack/src/main/java/br/com/oncast/ontrack/client/ui/places/login/LoginPanel.java
@@ -5,6 +5,7 @@ import static br.com.oncast.ontrack.client.utils.keyboard.BrowserKeyCodes.KEY_EN
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.logical.shared.AttachEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
@@ -50,6 +51,12 @@ public class LoginPanel extends Composite implements LoginView {
 	protected void passwordAreaOnKeyUp(final KeyUpEvent event) {
 		if (event.getNativeKeyCode() != KEY_ENTER) return;
 		doAuthenticate();
+	}
+
+	@UiHandler("emailArea")
+	protected void onAttach(final AttachEvent event) {
+		if (!event.isAttached()) return;
+		emailArea.setFocus(true);
 	}
 
 	@UiHandler("emailArea")
