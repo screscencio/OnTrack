3138f6c8b497aa049a81c692e604c388c507c5c5
Renato Besen <renato.besen@oncast.com.br>
2012-09-11 09:50:34 +0000

    Remove GwtComet related files and configuration

--- ontrack/src/main/java/br/com/oncast/ontrack/client/services/serverPush/GwtCometPingServiceAsync.java
+++
@@ -1,8 +0,0 @@
-package br.com.oncast.ontrack.client.services.serverPush;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface GwtCometPingServiceAsync {
-
-	void ping(AsyncCallback<Void> callback);
-}
