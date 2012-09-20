32fa357be70da3758609fba7d7147f49ffc61398
saulo <saulo.venancio@oncast.com.br>
2012-09-20 10:54:45 -0300

    ontrack development environment creation

---
+++ ontrack/scripts/ontrackScript.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mkdir ~/Development
+mkdir ~/workspace
+mkdir ~/Development/lib
+
+sudo apt-get install -y openjdk-6-jdk git chromium-browser unzip maven2
+
+cd ~/Development
+
+wget "http://eclipse.c3sl.ufpr.br/eclipse/downloads/drops4/R-4.2-201206081400/eclipse-SDK-4.2-linux-gtk.tar.gz"
+tar -xvf eclipse-SDK-4.2-linux-gtk.tar.gz
+rm eclipse-SDK-4.2-linux-gtk.tar.gz
+
+ln -s ~/Development/eclipse/eclipse ~/Desktop/eclipse
+
+cd lib
+wget "http://google-web-toolkit.googlecode.com/files/gwt-2.4.0.zip"
+unzip gwt-2.4.0.zip
+rm gwt-2.4.0.zip
+
+ln -s gwt-2.4.0 gwt
+
