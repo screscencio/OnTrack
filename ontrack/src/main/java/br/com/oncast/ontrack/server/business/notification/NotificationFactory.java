573ffcde0a6ca2d04836686483add6fc55268006
Luiz Paulo de Farias Jr <luiz.farias@oncast.com.br>
2012-10-10 11:20:08 -0300

    Sending description to progress change notification

--- ontrack/src/main/java/br/com/oncast/ontrack/server/business/notification/NotificationFactory.java
+++ ontrack/src/main/java/br/com/oncast/ontrack/server/business/notification/NotificationFactory.java
@@ -2,12 +2,9 @@ package br.com.oncast.ontrack.server.business.notification;
 
 import java.util.List;
 
-import br.com.oncast.ontrack.server.business.ServerServiceProvider;
 import br.com.oncast.ontrack.server.services.persistence.PersistenceService;
 import br.com.oncast.ontrack.server.services.persistence.exceptions.NoResultFoundException;
 import br.com.oncast.ontrack.server.services.persistence.exceptions.PersistenceException;
-import br.com.oncast.ontrack.shared.exceptions.business.ProjectNotFoundException;
-import br.com.oncast.ontrack.shared.exceptions.business.UnableToLoadProjectException;
 import br.com.oncast.ontrack.shared.exceptions.business.UnableToPostProcessActionException;
 import br.com.oncast.ontrack.shared.model.action.ActionContext;
 import br.com.oncast.ontrack.shared.model.action.AnnotationCreateAction;
@@ -15,7 +12,6 @@ import br.com.oncast.ontrack.shared.model.action.ImpedimentCreateAction;
 import br.com.oncast.ontrack.shared.model.action.ImpedimentSolveAction;
 import br.com.oncast.ontrack.shared.model.action.ModelAction;
 import br.com.oncast.ontrack.shared.model.action.ScopeDeclareProgressAction;
-import br.com.oncast.ontrack.shared.model.project.Project;
 import br.com.oncast.ontrack.shared.model.project.ProjectContext;
 import br.com.oncast.ontrack.shared.model.project.ProjectRepresentation;
 import br.com.oncast.ontrack.shared.model.scope.exceptions.ScopeNotFoundException;
@@ -29,34 +25,35 @@ public class NotificationFactory {
 	private enum ActionNotificationCreator {
 		IMPEDIMENT_CREATION(ImpedimentCreateAction.class) {
 			@Override
-			protected NotificationBuilder createNotificationBuilder(final ModelAction action, final ProjectRepresentation projectRepresentation,
+			protected NotificationBuilder createNotificationBuilder(final ModelAction action, final ProjectContext projectContext,
 					final User author) {
 
-				return initializeBuilder(action, projectRepresentation, author, NotificationType.IMPEDIMENT_CREATED);
+				return initializeBuilder(action, projectContext.getProjectRepresentation(), author, NotificationType.IMPEDIMENT_CREATED);
 			}
 		},
 		IMPEDIMENT_COMPLETITION(ImpedimentSolveAction.class) {
 			@Override
-			protected NotificationBuilder createNotificationBuilder(final ModelAction action, final ProjectRepresentation projectRepresentation,
+			protected NotificationBuilder createNotificationBuilder(final ModelAction action, final ProjectContext projectContext,
 					final User author) {
 
-				return initializeBuilder(action, projectRepresentation, author, NotificationType.IMPEDIMENT_SOLVED);
+				return initializeBuilder(action, projectContext.getProjectRepresentation(), author, NotificationType.IMPEDIMENT_SOLVED);
 			}
 		},
 		PROGRESS_DECLARED(ScopeDeclareProgressAction.class) {
 			@Override
-			protected NotificationBuilder createNotificationBuilder(final ModelAction action, final ProjectRepresentation projectRepresentation,
+			protected NotificationBuilder createNotificationBuilder(final ModelAction action, final ProjectContext projectContext,
 					final User author) {
 
-				return initializeBuilder(action, projectRepresentation, author, NotificationType.PROGRESS_DECLARED);
+				return initializeBuilder(action, projectContext.getProjectRepresentation(), author, NotificationType.PROGRESS_DECLARED).setDescription(
+						getScopeDescriptionFor(action, projectContext));
 			}
 		},
 		ANNOTATION_CREATED(AnnotationCreateAction.class) {
 			@Override
-			protected NotificationBuilder createNotificationBuilder(final ModelAction action, final ProjectRepresentation projectRepresentation,
+			protected NotificationBuilder createNotificationBuilder(final ModelAction action, final ProjectContext projectContext,
 					final User author) {
 
-				return initializeBuilder(action, projectRepresentation, author, NotificationType.ANNOTATION_CREATED);
+				return initializeBuilder(action, projectContext.getProjectRepresentation(), author, NotificationType.ANNOTATION_CREATED);
 			}
 		};
 
@@ -66,18 +63,9 @@ public class NotificationFactory {
 			return new NotificationBuilder(type, projectRepresentation, author).setReferenceId(action.getReferenceId());
 		}
 
-		private static String getScopeDescriptionFor(final ModelAction action, final ProjectRepresentation projectRepresentation) {
+		private static String getScopeDescriptionFor(final ModelAction action, final ProjectContext projectContext) {
 			try {
-
-				final Project project = ServerServiceProvider.getInstance().getBusinessLogic().loadProject(projectRepresentation.getId());
-
-				return new ProjectContext(project).findScope(action.getReferenceId()).getDescription();
-			}
-			catch (final ProjectNotFoundException e1) {
-				throw new UnableToPostProcessActionException("It was not possible to create new notification builder.", e1);
-			}
-			catch (final UnableToLoadProjectException e2) {
-				throw new UnableToPostProcessActionException("It was not possible to create new notification builder.", e2);
+				return projectContext.findScope(action.getReferenceId()).getDescription();
 			}
 			catch (final ScopeNotFoundException e3) {
 				throw new UnableToPostProcessActionException("It was not possible to create new notification builder.", e3);
@@ -98,7 +86,7 @@ public class NotificationFactory {
 			return null;
 		}
 
-		protected abstract NotificationBuilder createNotificationBuilder(ModelAction action, ProjectRepresentation projectRepresentation, User author);
+		protected abstract NotificationBuilder createNotificationBuilder(ModelAction action, ProjectContext projectContext, User author);
 	}
 
 	private final PersistenceService persistenceService;
@@ -117,7 +105,7 @@ public class NotificationFactory {
 			final List<User> projectUsers = persistenceService.retrieveProjectUsers(projectRepresentation);
 			final User author = persistenceService.retrieveUserByEmail(actionContext.getUserEmail());
 
-			final NotificationBuilder notificationBuilder = creator.createNotificationBuilder(action, projectRepresentation, author);
+			final NotificationBuilder notificationBuilder = creator.createNotificationBuilder(action, projectContext, author);
 
 			for (final User user : projectUsers) {
 				notificationBuilder.addReceipient(user);
