8fceff8c6f6e493d9f28014b4cf81d215dac167c
rhlobo <rhlobo+github@gmail.com>
2012-10-18 15:37:24 -0300

    Ignored some generated files; Removed deprecated upload script; Created activity factory.

---
+++ ontrack/src/main/java/br/com/oncast/ontrack/client/ui/places/AppActivityFactory.java
@@ -0,0 +1,64 @@
+package br.com.oncast.ontrack.client.ui.places;
+
+import br.com.oncast.ontrack.client.ui.places.details.DetailActivity;
+import br.com.oncast.ontrack.client.ui.places.details.DetailPlace;
+import br.com.oncast.ontrack.client.ui.places.loading.ContextLoadingActivity;
+import br.com.oncast.ontrack.client.ui.places.loading.ServerPushLoadingActivity;
+import br.com.oncast.ontrack.client.ui.places.loading.UserInformationLoadingActivity;
+import br.com.oncast.ontrack.client.ui.places.login.LoginActivity;
+import br.com.oncast.ontrack.client.ui.places.login.LoginPlace;
+import br.com.oncast.ontrack.client.ui.places.organization.OrganizationActivity;
+import br.com.oncast.ontrack.client.ui.places.organization.OrganizationPlace;
+import br.com.oncast.ontrack.client.ui.places.planning.PlanningActivity;
+import br.com.oncast.ontrack.client.ui.places.planning.PlanningPlace;
+import br.com.oncast.ontrack.client.ui.places.progress.ProgressActivity;
+import br.com.oncast.ontrack.client.ui.places.progress.ProgressPlace;
+import br.com.oncast.ontrack.client.ui.places.projectCreation.ProjectCreationActivity;
+import br.com.oncast.ontrack.client.ui.places.projectCreation.ProjectCreationPlace;
+import br.com.oncast.ontrack.client.ui.places.projectSelection.ProjectSelectionActivity;
+
+import com.google.gwt.activity.shared.Activity;
+import com.google.gwt.place.shared.Place;
+
+public class AppActivityFactory {
+
+	protected Activity getOrganizationActivity(final OrganizationPlace place) {
+		return new OrganizationActivity(place);
+	}
+
+	protected Activity getConnectServerPushLoadingActivity(final Place place) {
+		return new ServerPushLoadingActivity(place);
+	}
+
+	protected Activity getDetailActivity(final DetailPlace place) {
+		return new DetailActivity(place);
+	}
+
+	protected Activity getProgressActivity(final ProgressPlace place) {
+		return new ProgressActivity(place);
+	}
+
+	protected Activity getProjectCreationPlace(final ProjectCreationPlace place) {
+		return new ProjectCreationActivity(place);
+	}
+
+	protected Activity getProjectSelectionActivity() {
+		return new ProjectSelectionActivity();
+	}
+
+	protected Activity getLoginActivity(final LoginPlace loginPlace) {
+		return new LoginActivity(loginPlace.getDestinationPlace());
+	}
+
+	protected PlanningActivity getPlanningActivity(final PlanningPlace place) {
+		return new PlanningActivity(place);
+	}
+
+	protected ContextLoadingActivity getContextLoadingActivity(final ProjectDependentPlace projectDependentPlace) {
+		return new ContextLoadingActivity(projectDependentPlace);
+	}
+
+	protected UserInformationLoadingActivity getUserInformationLoadingActivity(final Place place) {
+		return new UserInformationLoadingActivity(place);
+	}
+}
--- ontrack/src/main/java/br/com/oncast/ontrack/client/ui/places/AppActivityMapper.java
+++ ontrack/src/main/java/br/com/oncast/ontrack/client/ui/places/AppActivityMapper.java
@@ -1,22 +1,12 @@
 package br.com.oncast.ontrack.client.ui.places;
 
 import br.com.oncast.ontrack.client.services.ClientServiceProvider;
-import br.com.oncast.ontrack.client.ui.places.details.DetailActivity;
 import br.com.oncast.ontrack.client.ui.places.details.DetailPlace;
-import br.com.oncast.ontrack.client.ui.places.loading.ContextLoadingActivity;
-import br.com.oncast.ontrack.client.ui.places.loading.ServerPushLoadingActivity;
-import br.com.oncast.ontrack.client.ui.places.loading.UserInformationLoadingActivity;
-import br.com.oncast.ontrack.client.ui.places.login.LoginActivity;
 import br.com.oncast.ontrack.client.ui.places.login.LoginPlace;
-import br.com.oncast.ontrack.client.ui.places.organization.OrganizationActivity;
 import br.com.oncast.ontrack.client.ui.places.organization.OrganizationPlace;
-import br.com.oncast.ontrack.client.ui.places.planning.PlanningActivity;
 import br.com.oncast.ontrack.client.ui.places.planning.PlanningPlace;
-import br.com.oncast.ontrack.client.ui.places.progress.ProgressActivity;
 import br.com.oncast.ontrack.client.ui.places.progress.ProgressPlace;
-import br.com.oncast.ontrack.client.ui.places.projectCreation.ProjectCreationActivity;
 import br.com.oncast.ontrack.client.ui.places.projectCreation.ProjectCreationPlace;
-import br.com.oncast.ontrack.client.ui.places.projectSelection.ProjectSelectionActivity;
 import br.com.oncast.ontrack.client.ui.places.projectSelection.ProjectSelectionPlace;
 import br.com.oncast.ontrack.shared.model.uuid.UUID;
 
@@ -27,74 +17,37 @@ import com.google.gwt.place.shared.Place;
 public class AppActivityMapper implements ActivityMapper {
 
 	private final ClientServiceProvider services;
+	private final AppActivityFactory activityFactory;
 
 	public AppActivityMapper(final ClientServiceProvider serviceProvider) {
 		this.services = serviceProvider;
+		this.activityFactory = new AppActivityFactory();
 	}
 
 	@Override
 	public Activity getActivity(final Place place) {
-		if (!services.getServerPushClientService().isConnected()) return connectServerPushLoadingActivity(place);
+		if (!services.getServerPushClientService().isConnected()) return activityFactory.getConnectServerPushLoadingActivity(place);
 
-		if (place instanceof LoginPlace) return createLoginActivity((LoginPlace) place);
+		if (place instanceof LoginPlace) return activityFactory.getLoginActivity((LoginPlace) place);
 
-		if (!services.getAuthenticationService().isUserAvailable()) return createUserInformationLoadingActivity(place);
+		if (!services.getAuthenticationService().isUserAvailable()) return activityFactory.getUserInformationLoadingActivity(place);
 
 		if (place instanceof ProjectDependentPlace) {
 			final ProjectDependentPlace projectDependentPlace = (ProjectDependentPlace) place;
 			final UUID requestedProjectId = projectDependentPlace.getRequestedProjectId();
 
-			if (requestedProjectId == null || !requestedProjectId.isValid()) return createProjectSelectionActivity();
-			if (!services.getContextProviderService().isContextAvailable(requestedProjectId)) return createContextLoadingActivity(projectDependentPlace);
+			if (requestedProjectId == null || !requestedProjectId.isValid()) return activityFactory.getProjectSelectionActivity();
+			if (!services.getContextProviderService().isContextAvailable(requestedProjectId)) return activityFactory
+					.getContextLoadingActivity(projectDependentPlace);
 		}
 
-		if (place instanceof DetailPlace) return createDetailActivity((DetailPlace) place);
-		if (place instanceof PlanningPlace) return createPlanningActivity((PlanningPlace) place);
-		if (place instanceof ProjectSelectionPlace) return createProjectSelectionActivity();
-		if (place instanceof ProjectCreationPlace) return createProjectCreationPlace((ProjectCreationPlace) place);
-		if (place instanceof ProgressPlace) return createProgressActivity((ProgressPlace) place);
-		if (place instanceof OrganizationPlace) return createOrganizationActivity((OrganizationPlace) place);
+		if (place instanceof DetailPlace) return activityFactory.getDetailActivity((DetailPlace) place);
+		if (place instanceof PlanningPlace) return activityFactory.getPlanningActivity((PlanningPlace) place);
+		if (place instanceof ProjectSelectionPlace) return activityFactory.getProjectSelectionActivity();
+		if (place instanceof ProjectCreationPlace) return activityFactory.getProjectCreationPlace((ProjectCreationPlace) place);
+		if (place instanceof ProgressPlace) return activityFactory.getProgressActivity((ProgressPlace) place);
+		if (place instanceof OrganizationPlace) return activityFactory.getOrganizationActivity((OrganizationPlace) place);
 
 		return null;
-	}
-
-	private Activity createOrganizationActivity(final OrganizationPlace place) {
-		return new OrganizationActivity(place);
-	}
-
-	private Activity connectServerPushLoadingActivity(final Place place) {
-		return new ServerPushLoadingActivity(place);
-	}
-
-	private Activity createDetailActivity(final DetailPlace place) {
-		return new DetailActivity(place);
-	}
-
-	private Activity createProgressActivity(final ProgressPlace place) {
-		return new ProgressActivity(place);
-	}
-
-	private Activity createProjectCreationPlace(final ProjectCreationPlace place) {
-		return new ProjectCreationActivity(place);
-	}
-
-	private Activity createProjectSelectionActivity() {
-		return new ProjectSelectionActivity();
-	}
-
-	private Activity createLoginActivity(final LoginPlace loginPlace) {
-		return new LoginActivity(loginPlace.getDestinationPlace());
-	}
-
-	private PlanningActivity createPlanningActivity(final PlanningPlace place) {
-		return new PlanningActivity(place);
-	}
-
-	private ContextLoadingActivity createContextLoadingActivity(final ProjectDependentPlace projectDependentPlace) {
-		return new ContextLoadingActivity(projectDependentPlace);
-	}
-
-	private UserInformationLoadingActivity createUserInformationLoadingActivity(final Place place) {
-		return new UserInformationLoadingActivity(place);
 	}
 }
