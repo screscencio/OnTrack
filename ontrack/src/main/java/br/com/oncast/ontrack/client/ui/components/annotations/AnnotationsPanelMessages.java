8fe2bcfdd81e55577d1bbc83f85e9e7204eab554
Luiz Paulo de Farias Jr <luiz.farias@oncast.com.br>
2012-10-29 17:30:50 -0200

    Double click to edit annotation title

---
+++ ontrack/src/main/java/br/com/oncast/ontrack/client/ui/components/annotations/AnnotationsPanelMessages.java
@@ -0,0 +1,11 @@
+package br.com.oncast.ontrack.client.ui.components.annotations;
+
+import br.com.oncast.ontrack.client.i18n.BaseMessages;
+
+public interface AnnotationsPanelMessages extends BaseMessages {
+
+	@Description("double click to edit annotation title")
+	@DefaultMessage("Double click to edit")
+	String doubleClickToEdit();
+
+}
