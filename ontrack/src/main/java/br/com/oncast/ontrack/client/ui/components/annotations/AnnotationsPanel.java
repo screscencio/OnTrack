aaffeb12a6395c28e639e5f86b37d5a2ba3cb525
Luiz Paulo de Farias Jr <luiz.farias@oncast.com.br>
2012-10-29 17:30:50 -0200

    Double click to edit annotation title

--- ontrack/src/main/java/br/com/oncast/ontrack/client/ui/components/annotations/AnnotationsPanel.java
+++ ontrack/src/main/java/br/com/oncast/ontrack/client/ui/components/annotations/AnnotationsPanel.java
@@ -93,7 +93,6 @@ public class AnnotationsPanel extends Composite implements HasCloseHandlers<Anno
 		initWidget(uiBinder.createAndBindUi(this));
 
 		checklist.setSubjectId(subjectId);
-
 		this.subjectTitle.setValue(subjectDescription);
 	}
 
