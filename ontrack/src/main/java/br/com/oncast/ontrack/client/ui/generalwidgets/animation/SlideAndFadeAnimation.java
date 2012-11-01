6bf49c47ce4b400b3e37f12d4cc589f1b7a44c60
rhlobo <rhlobo+github@gmail.com>
2012-11-01 11:11:34 -0200

    Animations mostly done

--- ontrack/src/main/java/br/com/oncast/ontrack/client/ui/generalwidgets/animation/SlideAndFadeAnimation.java
+++ ontrack/src/main/java/br/com/oncast/ontrack/client/ui/generalwidgets/animation/SlideAndFadeAnimation.java
@@ -7,8 +7,8 @@ import com.google.gwt.user.client.ui.Widget;
 public class SlideAndFadeAnimation implements ShowAnimation, HideAnimation {
 
 	private final Widget widget;
-	private static final int DEFAULT_SLIDE_DURATION = 4000;
-	private static final int DEFAULT_FADE_DURATION = 5000;
+	private static final int DEFAULT_SLIDE_DURATION = 400;
+	private static final int DEFAULT_FADE_DURATION = 600;
 
 	public SlideAndFadeAnimation(final Widget widget) {
 		this.widget = widget;
@@ -21,7 +21,20 @@ public class SlideAndFadeAnimation implements ShowAnimation, HideAnimation {
 
 	@Override
 	public void show(final AnimationCallback callback) {
-		getWidgetJqueryElement().clearQueue().hide().fadeTo(1, 0).slideDown(DEFAULT_SLIDE_DURATION).fadeTo(DEFAULT_FADE_DURATION, 1, callback);
+		final JQuery $ = getWidgetJqueryElement();
+		$.clearQueue().fadeTo(0, 0, new AnimationCallback() {
+
+			@Override
+			public void onComplete() {
+				$.hide().slideDown(DEFAULT_SLIDE_DURATION, new AnimationCallback() {
+
+					@Override
+					public void onComplete() {
+						$.fadeTo(DEFAULT_FADE_DURATION, 1, callback);
+					}
+				});
+			}
+		});
 	}
 
 	@Override
@@ -31,7 +44,14 @@ public class SlideAndFadeAnimation implements ShowAnimation, HideAnimation {
 
 	@Override
 	public void hide(final AnimationCallback callback) {
-		getWidgetJqueryElement().clearQueue().fadeTo(DEFAULT_FADE_DURATION, 0).slideUp(DEFAULT_SLIDE_DURATION, callback);
+		final JQuery $ = getWidgetJqueryElement();
+		$.clearQueue().fadeTo(DEFAULT_FADE_DURATION, 0, new AnimationCallback() {
+
+			@Override
+			public void onComplete() {
+				$.slideUp(DEFAULT_SLIDE_DURATION, callback);
+			}
+		});
 	}
 
 	private JQuery getWidgetJqueryElement() {
