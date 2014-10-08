2f608e4cfa2976311d684fcea2948b95448e8206
Celso Martins <celso.av.martins@gmail.com>
2014-10-08 13:18:43 -0300

    testing listeners by reflection

--- ontrack/src/test/java/br/com/oncast/ontrack/client/services/user/UsersStatusServiceImplTest.java
+++ ontrack/src/test/java/br/com/oncast/ontrack/client/services/user/UsersStatusServiceImplTest.java
@@ -6,15 +6,23 @@ import br.com.oncast.ontrack.client.services.context.ContextProviderService;
 import br.com.oncast.ontrack.client.services.serverPush.ServerPushClientService;
 import br.com.oncast.ontrack.client.services.user.UsersStatusServiceImpl.UsersStatusChangeListener;
 
+import java.lang.reflect.Field;
+import java.util.Set;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.web.bindery.event.shared.EventBus;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+@RunWith(MockitoJUnitRunner.class)
 public class UsersStatusServiceImplTest {
 
 	@Mock
@@ -32,11 +40,38 @@ public class UsersStatusServiceImplTest {
 	@Mock
 	private UsersStatusChangeListener usersStatusChangeListener;
 
+	private UsersStatusServiceImpl service;
+
+	@Before
+	public void setup() {
+		service = new UsersStatusServiceImpl(requestDispatchService, contextProviderService, serverPushClientService, eventBus);
+	}
+
 	@Test
 	public void shouldRegisterServerPushEventHandlers() {
-		MockitoAnnotations.initMocks(this);
-		final UsersStatusServiceImpl service = new UsersStatusServiceImpl(requestDispatchService, contextProviderService, serverPushClientService, eventBus);
-		final HandlerRegistration registration = service.register(usersStatusChangeListener);
-		assertNotNull(registration);
+		final HandlerRegistration handler = service.register(usersStatusChangeListener);
+		assertNotNull(handler);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void shouldAddListenerOnRegister() throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException {
+		final Field field = service.getClass().getDeclaredField("listenersList");
+		field.setAccessible(true);
+		final Set<UsersStatusChangeListener> listenersList = (Set<UsersStatusChangeListener>) field.get(service);
+		service.register(usersStatusChangeListener);
+		assertFalse(listenersList.isEmpty());
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void shouldBeAbleToRemoveListener() throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException {
+		final Field field = service.getClass().getDeclaredField("listenersList");
+		field.setAccessible(true);
+		final Set<UsersStatusChangeListener> listenersList = (Set<UsersStatusChangeListener>) field.get(service);
+		final HandlerRegistration handler = service.register(usersStatusChangeListener);
+		assertFalse(listenersList.isEmpty());
+		handler.removeHandler();
+		assertTrue(listenersList.isEmpty());
 	}
 }
