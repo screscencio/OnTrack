8262e993c9d625441bdbf5a83f5424cdd98896e9
Celso Martins <celso.av.martins@gmail.com>
2014-10-20 22:06:28 -0200

    now onboarding url is get dynamically. changed project select panel message too

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
 OnTrack
 
+Development environment
 - Java 1.7 <
 - GWT 2.5.1
 - Eclipse Kepler(4.3) <
 - AJDT Eclipse Plugin
 - GWT Eclipse Plugin
+
+- user: admin@ontrack.com
+- pass: admin
+
+
+If you want a manual package generation, please run
+mvn package -Pprod -Dmaven.test.skip=true
+
+and if you want back to development configuration, run again
+mvn clean package -Dmaven.test.skip=true
+
+In one of it steps, maven get the environment.prod file and copy it to
+the configurantion folder. So you keep your production configuration
+even if you build and run inside eclipse.
--- ontrack/src/main/java/br/com/oncast/ontrack/server/services/api/OnTrackOnboardingResource.java
+++ ontrack/src/main/java/br/com/oncast/ontrack/server/services/api/OnTrackOnboardingResource.java
@@ -1,13 +1,14 @@
 package br.com.oncast.ontrack.server.services.api;
 
 import br.com.oncast.ontrack.server.business.ServerServiceProvider;
-import br.com.oncast.ontrack.server.configuration.Configurations;
 import br.com.oncast.ontrack.server.services.api.bean.OnboardingApiRequest;
 import br.com.oncast.ontrack.shared.model.user.Profile;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 @Path("/onboarding")
@@ -16,9 +17,11 @@ public class OnTrackOnboardingResource {
 	@POST
 	@Path("/user")
 	@Consumes({ MediaType.APPLICATION_FORM_URLENCODED, MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-	public String onboarding(final OnboardingApiRequest request) {
+	public String onboarding(final OnboardingApiRequest request, @Context final HttpServletRequest req) {
+		final StringBuffer url = req.getRequestURL();
+		final String baseURL = url.substring(0, url.length() - req.getRequestURI().length()) + req.getContextPath() + "/";
 		final String accessToken = ServerServiceProvider.getInstance().getBusinessLogic().createTrialUser(request.getEmail(), Profile.ACCOUNT_MANAGER).toString();
-		return Configurations.get().getApplicationBaseUrl() + "onboarding/access/" + accessToken;
+		final String onboard = baseURL + "onboarding/access/" + accessToken;
+		return onboard;
 	}
-
 }
